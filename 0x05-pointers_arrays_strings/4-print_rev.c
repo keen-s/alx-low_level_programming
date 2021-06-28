5294edd3b9b67e3472b90d79dcdb90ec0bf6e2d3
keen-spe <keen_spe@DESKTOP-C7LRBS8.localdomain>
2021-06-28 16:49:38 +0200

    New

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,3 +13,16 @@ int _strlen(char *s)
 		s++;
 	return (s - p);
 }
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string s
+ */
+void print_rev(char *s)
+{
+	int size = _strlen(s);
+
+	while (size--)
+		_putchar(*(s + size));
+	_putchar('\n');
+}
