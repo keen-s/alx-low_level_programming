2687fe6400f662f0cc41979a548711e10c57b2fc
keen-spe <keen_spe@DESKTOP-C7LRBS8.localdomain>
2021-08-05 15:44:38 +0200

    new file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,61 +1,23 @@
 #include "main.h"
 
 /**
- * powX - raises b to p
- * @b: base
- * @p: power
- * Return: b the power of p
- */
-int powX(int b, int p)
-{
-	int prod = 1;
-
-	while (p > 0)
-	{
-		prod *= b;
-		p--;
-	}
-	return (prod);
-}
-
-/**
- * _len - length of a string
- * @s:string
- * Return: lenght of s
- */
-int _len(const char *s)
-{
-	int len = 0;
-
-	while (*s)
-	{
-		len++;
-		s++;
-	}
-	return (len);
-}
-/**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: input string
- * Return: unsigned decimal conversion, 0 if input is NULL
+ * binary_to_uint - convert a binary to an unsigned int
+ * @b: a pointer to a string of 0 and 1
+ * Return: the converted number or 0 if fails
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int power;
-	int num = 0;
+	unsigned int ret = 0;
+	int i, j;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
-	power = _len(b) - 1;
-	while (*b)
+	for (i = 0; b[i + 1]; i++)
 	{
-		if (*b != '0' && *b != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-
-		if (*b == '1')
-			num += powX(2, power);
-		b++;
-		power--;
 	}
-	return (num);
+	for (j = 1; i >= 0; i--, j *= 2)
+		ret += ((b[i] - '0') * (j));
+	return (ret);
 }
